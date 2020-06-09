b40bc2a7d7092ac35c0ed453161bae3d96a2f42b
Hasham Ahmad <mr.hash@gmail.com>
2020-06-09 10:03:26 -0500

    Replace trait with parent map

--- src/Service/MoneyServiceMap.php
+++ src/Service/MoneyServiceMap.php
@@ -8,13 +8,10 @@
 
 namespace Daikon\Money\Service;
 
-use Daikon\DataStructure\TypedMapInterface;
-use Daikon\DataStructure\TypedMapTrait;
+use Daikon\DataStructure\TypedMap;
 
-final class MoneyServiceMap implements TypedMapInterface
+final class MoneyServiceMap extends TypedMap
 {
-    use TypedMapTrait;
-
     public function __construct(iterable $services = [])
     {
         $this->init($services, [MoneyServiceInterface::class]);
