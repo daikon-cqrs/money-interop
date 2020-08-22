f7cf63b363a7d5455fd53593769c43e6dee18140
Hasham Ahmad <mr.hash@gmail.com>
2020-08-22 18:24:01 -0500

    Add payment service exceptions

---
+++ src/Exception/PaymentServiceFailed.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the daikon-cqrs/money-interop project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Daikon\Money\Exception;
+
+use Daikon\Interop\RuntimeException;
+
+class PaymentServiceFailed extends RuntimeException implements PaymentServiceException
+{
+}
